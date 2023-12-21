95bd54932fb215a5b5507cfb3e1292f01cf657a9
emma-33 <emma.lateyron@orange.fr>
2023-12-21 15:01:05 +0100

    update man

--- loop.c
+++ loop.c
@@ -19,11 +19,13 @@ int loop(void)
 		if (count_char == -1)
 		{
 			free(line_ptr);
+			line_ptr = NULL;
 			exit(EXIT_SUCCESS);
 		}
 		if (strcmp(line_ptr, "exit\n") == 0)
 		{
 			free(line_ptr);
+			line_ptr = NULL;
 			exit(EXIT_SUCCESS);
 		}
 		if (strcmp(line_ptr, "env\n") == 0)
@@ -32,7 +34,10 @@ int loop(void)
 		}
 		ex = _wait(line_ptr);
 		if (ex == -1)
+		{
 			perror("./hsh");
+			free(line_ptr);
+		}
 		free(line_ptr);
 		line_ptr = NULL;
 	}
